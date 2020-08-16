0d27fcc56344f22964b6a975bef0c0d9ce699a9b
neslinur <snsoykan13@gmail.com>
2020-08-16 16:01:50 +0300

    Fix wording

--- bin/index.js
+++ bin/index.js
@@ -5,7 +5,7 @@ const saveCounts = require("../src/services/db-operations/saveCounts");
 const saveMaster = require("../src/services/db-operations/saveMaster");
 const generateLocBarAmnt = require("../src/services/generate-reports/locBarAmnt");
 const generateBarAmnt = require("../src/services/generate-reports/barAmnt");
-const generateAggregate = require("../src/services/generate-reports/aggregate");
+const generateAggregated = require("../src/services/generate-reports/aggregate");
 
 //first, call saveCounts, then saveMaster, they should be thenable, call one .then the other
 //then generate the reports, again, they're asynchronous
--- src/services/generate-reports/aggregate.js
+++ src/services/generate-reports/aggregate.js
@@ -2,4 +2,4 @@
 //map each item's completedCounts
 //map contents
 //for each item in the contents array, find where the current barcode matches the barcode of an item 
-//in the master collection. write the aggregate report using fs module into an aggregate.txt file in the reports folder
+//in the master collection. write the aggregated report using fs module into an aggregated.txt file in the reports folder
